f47eb111446a1ab321286e0de66596e77af4d1d5
DamonSeahash5 <oliverhopwood5@gmail.com>
2025-01-23 14:12:39 +0000

    set up initial repo using Vite

---
+++ src/components/flashcard/Flashcard.jsx
@@ -0,0 +1,11 @@
+//Question / answer from State array is the prop for each flashcard
+
+//Create flashcard React component
+    //onClick, the card's class changes from question to answer and the text / style changes
+    
+    //return each flashcard as a div for now
+
+
+
+// Children of each flashcard:
+// - Question or answer <p>
